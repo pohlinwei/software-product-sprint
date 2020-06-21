d66f66d6c50a4d64d222b8adc995a03f3b1672d8
Lin Wei <linwei0909@hotmail.com>
2020-06-21 17:35:13 +0800

    Add main function

---
+++ portfolio/src/main/webapp/projects/commentart/main.js
@@ -0,0 +1,18 @@
+/** Sets up all the required listeners and creates all necessary elements. */
+const main = () => {
+  const body = document.getElementsByTagName('body')[0];
+  body.onload = fetchAndShowComments;
+}
+
+/** Fetches comments from the server. */
+const fetchAndShowComments= () => {
+  fetch('/read_comments')
+      .then(response => response.json())
+      .then(commentsJson => {
+        const comments = commentsJson.map(Comment.toComment(commentJson));
+        const commentsManager = new CommentsManager(comments);
+        enableAsyncComment(commentsManager);
+      });
+}
+
+main();
